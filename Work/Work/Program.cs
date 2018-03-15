6d04f7bbbd6785480cc0a70afeda377fe5b9a59a
LAPTOP-9RC7HJDL\陳柏霖 <aa02710@gmail.com>
2018-03-15 18:12:57 +0800

    第一次上傳

---
+++ Work/Work/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("廢物");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ test1/test1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+
+            // 宣告變數
+
+            Console.Write("請輸入身高:");
+            string tall = Console.ReadLine();
+            Console.Write("請輸入體重:");
+            string weight = Console.ReadLine();
+            double BMI;
+
+            BMI = double.Parse(weight) / (double.Parse(tall) / 100 * double.Parse(tall) / 100);
+            Console.WriteLine("您的BMI指數為" + BMI);
+            Console.ReadLine();
+        }
+    }
+}
