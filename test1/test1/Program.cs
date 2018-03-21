78752a71bae4e7df2b26f3c8dd879ff8e1aa5446
LAPTOP-9RC7HJDL\陳柏霖 <aa02710@gmail.com>
2018-03-21 14:47:38 +0800

    增加可讀性d(`･∀･)b 不知道要打多少

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -14,40 +14,85 @@ namespace test1
             Console.ForegroundColor = ConsoleColor.Red;
             Console.BackgroundColor = ConsoleColor.White;
 
-            // 宣告變數
-
+            // 宣告兩個變數 , 屬入身高體重
             Console.Write("請輸入身高:");
             float tall = float.Parse(Console.ReadLine()) / 100;
 
             Console.Write("請輸入體重:");
             float weight = float.Parse(Console.ReadLine());
 
+            // 宣告變數 , 判斷要檢測甚麼
+            Console.Write("輸入1為身體情形2為兵役狀況:");
+            int people = int.Parse(Console.ReadLine());
+
+            // 宣告變數 , 以便儲存計算後數值
             double BMI;
 
+            // 計算BMI值
             BMI = weight / (tall * tall);
 
-            if (BMI < 16.5 || BMI > 31.5)
+            // 當people = 1 , 進行以下程式碼
+            if (people == 1)
             {
-                Console.WriteLine("恭喜你不用當兵");
+                if (BMI < 18.5)
+                {
+                    Console.WriteLine("過輕！你需要再吃營養些，讓自己重一些！");
+                }
+
+                else if (18.5 <= BMI && BMI < 24)
+                {
+                    Console.WriteLine("標準！請您繼續保持！");
+                }
+
+                else if (24 <= BMI && BMI < 27)
+                {
+                    Console.WriteLine("過重！肥胖容易引起疾病，您得要多多注意自己的健康囉！");
+                }
+
+                else if (27 <= BMI && BMI < 30)
+                {
+                    Console.WriteLine("輕度肥胖！肥胖容易引起疾病，您得要多多注意自己的健康囉！");
+                }
+
+                else if (30 <= BMI && BMI < 35)
+                {
+                    Console.WriteLine("中度肥胖！肥胖容易引起疾病，您得要多多注意自己的健康囉！");
+                }
+
+                else
+                {
+                    Console.WriteLine("重度肥胖！肥胖容易引起疾病，您得要多多注意自己的健康囉！");
+                }
             }
 
-            else if (16.5 <= BMI && BMI < 17)
-            {
-                Console.WriteLine("你還是得當替代役");
-            }
-
-            else if (31 <= BMI && BMI < 31.5)
-            {
-                Console.WriteLine("你還是得當替代役");
-            }
-
+            // 若 people = 1 以外的值 , 進行以下程式碼
             else
             {
-                Console.WriteLine("乖乖當兵吧");
+                if (BMI < 16.5 || BMI > 31.5)
+                {
+                    Console.WriteLine("恭喜你不用當兵");
+                }
+
+                else if (16.5 <= BMI && BMI < 17)
+                {
+                    Console.WriteLine("你還是得當替代役");
+                }
+
+                else if (31 <= BMI && BMI < 31.5)
+                {
+                    Console.WriteLine("你還是得當替代役");
+                }
+
+                else
+                {
+                    Console.WriteLine("乖乖當兵吧");
+                }
             }
 
-            Console.WriteLine("您的BMI指數為" + BMI);
+            // 顯示結果
+            Console.WriteLine("您的BMI指數為" + BMI + "kg/m*m");
             Console.ReadLine();
+            
         }
     }
 }
