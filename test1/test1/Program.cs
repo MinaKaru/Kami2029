23e4fae6cca7f3282c1d4523e1640b6d705204d9
LAPTOP-9RC7HJDL\陳柏霖 <aa02710@gmail.com>
2018-04-25 23:06:40 +0800

    已修改

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -36,7 +36,7 @@ namespace test1
 
                 // 計算BMI值
                 BMI = weight / (tall * tall);
-
+               
                 // 當people = 1 , 進行以下程式碼
                 if (people == 1)
                 {
@@ -44,37 +44,32 @@ namespace test1
                     {
                         Console.WriteLine("過輕！你需要再吃營養些，讓自己重一些！");
                     }
-
                     else if (18.5 <= BMI && BMI < 24)
                     {
                         Console.WriteLine("標準！請您繼續保持！");
                     }
-
                     else if (24 <= BMI && BMI < 27)
                     {
                         Console.WriteLine("過重！肥胖容易引起疾病，您得要多多注意自己的健康囉！");
                         Console.Beep(1000, 1000);
                     }
-
                     else if (27 <= BMI && BMI < 30)
                     {
                         Console.WriteLine("輕度肥胖！肥胖容易引起疾病，您得要多多注意自己的健康囉！");
                         Console.Beep(1000, 1000);
                     }
-
                     else if (30 <= BMI && BMI < 35)
                     {
                         Console.WriteLine("中度肥胖！肥胖容易引起疾病，您得要多多注意自己的健康囉！");
                         Console.Beep(1000, 1000);
                     }
-
                     else
                     {
                         Console.WriteLine("重度肥胖！肥胖容易引起疾病，您得要多多注意自己的健康囉！");
                         Console.Beep(1000, 1000);
                     }
                 }
-
+               
                 // 若 people = 1 以外的值 , 進行以下程式碼
                 else
                 {
@@ -82,17 +77,14 @@ namespace test1
                     {
                         Console.WriteLine("恭喜你不用當兵");
                     }
-
                     else if (16.5 <= BMI && BMI < 17)
                     {
                         Console.WriteLine("你還是得當替代役");
                     }
-
                     else if (31 <= BMI && BMI < 31.5)
                     {
                         Console.WriteLine("你還是得當替代役");
                     }
-
                     else
                     {
                         Console.WriteLine("乖乖當兵吧");
